e474ae56b4cfe45aab27788b80ee978b85d74080
AwakexD <aleks.parvanow@gmail.com>
2024-03-02 22:32:38 +0200

    Test index view

---
+++ Web/Properly.Web.ViewModels/Home/IndexViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Properly.Web.ViewModels.Home
+{
+    using System.Collections.Generic;
+    using Properly.Web.ViewModels.Listing;
+    
+    public class IndexViewModel
+    {
+        public IndexViewModel()
+        {
+            this.Listings = new HashSet<ListingIndexViewModel>();
+        }
+
+        public int FeaturesCount { get; set; }
+
+        public IEnumerable<ListingIndexViewModel> Listings { get; set; }
+    }
+}
---
+++ Web/Properly.Web.ViewModels/Listing/ListingIndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Properly.Web.ViewModels.Listing
+{
+    using Properly.Data.Models;
+    using Properly.Services.Mapping;
+
+    public class ListingIndexViewModel
+    {
+        public int Price { get; set; }
+
+        public string Address { get; set; }
+    }
+}
