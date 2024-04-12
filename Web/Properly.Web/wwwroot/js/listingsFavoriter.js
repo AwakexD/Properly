8da16e367fa8795ac49e5e3c13104cec09af7a36
AwakexD <aleks.parvanow@gmail.com>
2024-04-13 00:20:30 +0300

    Added webApiRequester and listingsFavoriter, Button color change on click

---
+++ Web/Properly.Web/wwwroot/js/listingsFavoriter.js
@@ -0,0 +1,30 @@
+﻿import { post } from "./webApiRequester.js";
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const favouriteButtons = document.querySelectorAll("i.fa-bookmark");
+    const antiForgeryToken = document.querySelector("#antiForgeryForm input[name=__RequestVerificationToken]").value;
+
+    const addToFavouriteUrl = "/api/PropertyInteractions/Favourite"
+
+    favouriteButtons.forEach(element => {
+        element.addEventListener("click", (event) => {
+            event.preventDefault();
+            const listingId = event.target.closest(".body-main").querySelector("#listing-id").textContent;
+            AddToFavourites(listingId);
+            UpdateButtonColor(event);
+        })
+    })
+
+    async function AddToFavourites(listingId) {
+        await post(addToFavouriteUrl, { ListingId: listingId }, antiForgeryToken);
+    }
+
+
+    function UpdateButtonColor(event) {
+        const button = event.target;
+        button.style.transition = "color 0.3s ease";
+        const color = getComputedStyle(button)["color"];
+        const newColor = color === "rgb(255, 212, 59)" ? "rgb(116, 192, 252)" : "rgb(255, 212, 59)";
+        button.style.color = newColor;
+    }
+})
---
+++ Web/Properly.Web/wwwroot/js/webApiRequester.js
@@ -0,0 +1,52 @@
+﻿const domain = 'https://localhost:44319';
+
+
+async function requester(method, endPoints, data, token) {
+    const options = {
+        method,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: data ? JSON.stringify(data) : undefined,
+    };
+
+    if (token) {
+        options.headers['X-CSRF-TOKEN'] = token;
+    }
+
+    try {
+        const response = await fetch(domain + endPoints, options);
+
+        if (!response.ok) {
+            const error = await response.json();
+            throw new Error(error.message);
+        }
+
+        if (response.status === 204) {
+            return response;
+        }
+
+        const contentType = response.headers.get('content-type');
+        if (contentType && contentType.includes('application/json')) {
+            return await response.json();
+        }
+        return await response.text();
+
+    } catch (error) {
+        throw error;
+    }
+}
+
+const get = requester.bind(null, 'GET');
+const post = requester.bind(null, 'POST');
+const put = requester.bind(null, 'PUT');
+const patch = requester.bind(null, 'PATCH');
+const del = requester.bind(null, 'DELETE');
+
+export {
+    get,
+    post,
+    put,
+    patch,
+    del as delete,
+};
