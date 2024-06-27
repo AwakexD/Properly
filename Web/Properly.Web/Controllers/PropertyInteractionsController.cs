6fe8ef9b8c4a454832705a923283fa95662b6bf4
AwakexD <aleks.parvanow@gmail.com>
2024-06-27 22:42:47 +0300

    Temp alert remove

--- Web/Properly.Web/Controllers/PropertyInteractionsController.cs
+++ Web/Properly.Web/Controllers/PropertyInteractionsController.cs
@@ -1,17 +1,15 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Properly.Data.Models.User;
-using Properly.Web.ViewModels.Listing.Enums;
-
-namespace Properly.Web.Controllers
+﻿namespace Properly.Web.Controllers
 {
     using System;
-    using System.Security.Claims;
     using System.Threading.Tasks;
 
     using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
     using Properly.Common;
+    using Properly.Data.Models.User;
     using Properly.Services.Data.Contracts;
+    using Properly.Web.ViewModels.Listing.Enums;
     using Properly.Web.ViewModels.Listing;
 
     [ApiController]
@@ -51,17 +49,11 @@ namespace Properly.Web.Controllers
                 var user = await this.userManager.GetUserAsync(this.User);
                 await this.propertyService.ChangeListingStatus(user.Id, input.ListingId, ListingStatus.Sold);
 
-                TempData["Notification"] = ExceptionsAndNotificationsMessages.ListingWasUpdatedSuccessfully;
-                TempData["NotificationType"] = "success";
-
                 return this.RedirectToAction("Dashboard", "User");
 
             }
             catch (Exception e)
             {
-                TempData["Notification"] = ExceptionsAndNotificationsMessages.AnErrorOccurred;
-                TempData["NotificationType"] = "error";
-
                 return this.StatusCode(501, ExceptionsAndNotificationsMessages.AnErrorOccurred);
             }
         }
@@ -80,7 +72,7 @@ namespace Properly.Web.Controllers
             catch (Exception e)
             {
                 Console.Error.WriteLine(e);
-                return this.StatusCode(501, e.Message);
+                return this.StatusCode(501, ExceptionsAndNotificationsMessages.AnErrorOccurred);
             }
 
 
